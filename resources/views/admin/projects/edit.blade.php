071b76a9d757e04e10364d906960b912a9715774
FrancescaLoria <francesca.loria90@hotmail.it>
2023-06-07 18:01:33 +0200

    completed

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -17,6 +17,17 @@
             <label for="title" class="form-label">TITOLO</label>
             <input type="text" class="form-control" id="title" placeholder="scrivi il titolo" name="title" value="{{old('title', $project->title)}}">
         </div>
+
+        <div class="mb-3">
+            <label for="type">Tipologia</label>
+            <select class="form-select" id="type" name="type_id">
+                <option value=""></option>
+                @foreach ($types as $type)
+                    <option @selected($type->id == old('type_id',$project->type?->id)) value="{{ $type->id }}">{{ $type->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
         <div class="mb-3">
             <label for="content" class="form-label">CONTENUTO</label>
             <textarea class="form-control" id="content" name="content" rows="3">{{ old('content', $project->content)}}</textarea>
