15f1940760259fb5de59d5a96a23b69c7ab53e40
FrancescaLoria <francesca.loria90@hotmail.it>
2023-06-07 17:18:00 +0200

    add selects

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -16,6 +16,17 @@
             <label for="title" class="form-label">TITOLO</label>
             <input type="text" class="form-control" id="title" placeholder="scrivi il titolo" name="title" value="{{old('title')}}">
         </div>
+
+        <div class="mb-3">
+            <label for="type">Tipologia</label>
+            <select class="form-select" id="type" name="type_id">
+                <option value=""></option>
+                @foreach ($types as $type)
+                    <option @selected(old('type_id') == $type->id) value="{{ $type->id }}">{{ $type->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
         <div class="mb-3">
             <label for="content" class="form-label">CONTENUTO</label>
             <textarea class="form-control" id="content" name="content" rows="3">{{ old('content')}}</textarea>
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,7 +3,15 @@
 @section('content')
     <div class="container">
         <h1 class="title text-center">{{$project->title}}</h1>
-        <div class="text-end">{{$project->slug}}</div>
+        <div class="d-flex justify-content-between">
+            @if ($project->type)
+                <span>TIPOLOGIA: {{ $project->type->name }}</span>
+            @else
+                <span>NESSUNA TIPOLOGIA</span>
+            @endif
+            <span>{{ $project->slug }}</span>
+        </div>
+        
         <p class="mt-3">{{$project->content}}</p>
     </div>
     
